6d0188a4f53d01a7740cc875867116676eae8260
timothieecantcode <timothienguyen@gmail.com>
2026-09-16 14:53:35 +0930

    Configure router for Vercel deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.PROD ? '/hf-information-portal' : '/'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>

1a7e93de3bfc28bd6796740b434aeb27f14efffd
Peter Trinh <raihua.dev@gmail.com>
2026-09-17 06:52:44 +0930

    Add(cms): JWT Auth Schema

--- cms/Program.cs
+++ cms/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -14,16 +17,51 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:3000")
               .AllowAnyHeader()
+              .AllowCredentials()
               .AllowAnyMethod();
     });
 });
 
+var secret = builder.Configuration["Jwt:Secret"]
+    ?? throw new InvalidOperationException("Jwt:Secret is not configured");
+var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+builder.Services.AddAuthentication()
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+                ClockSkew = TimeSpan.FromSeconds(30)
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    if (string.IsNullOrEmpty(context.Token) 
+                    && context.Request.Cookies.TryGetValue("jwt", out var token))
+                    {
+                        context.Token = token;
+                    }
+
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
 WebApplication app = builder.Build();
+
 
 await app.BootUmbracoAsync();
 
 app.UseCors("ReactApp");
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseUmbraco()
     .WithMiddleware(u =>
     {
